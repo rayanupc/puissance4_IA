84d051cceeb8df2195a9bfe696d8b7f2e82d5653
Rayan Al Mohaize <rayan.almohaize@gmail.com>
2024-03-04 18:48:09 +0100

    chute des jetons (à continuer)

--- gui.py
+++ gui.py
@@ -1,5 +1,9 @@
-from constantes import NB_COLONNES, NB_LIGNES, TEMPS_CHUTE, LARGEUR_GRILLE, ESPACEMENT, RAYON
 import tkinter
+import time
+import threading  
+
+
+from constantes import *
 
 fenetreJeu = tkinter.Tk()
 fenetreJeu.title("Puissance 4")
@@ -23,9 +27,51 @@ def creation_grille(r):
                             ESPACEMENT + r + (ligne - 1) * (ESPACEMENT + 2 * r),
                             r, 'white', 'trou')
 
+def creation_jeton(colonne, ligne, c, r):  # Dépend de la grille
+    """Création d'un jeton de couleur c et de rayon r à la colonne et à la ligne indiquée"""
+    identifiant = creation_disque(colonne*(ESPACEMENT+2*r)-r,
+                                  (NB_LIGNES-ligne+1)*(ESPACEMENT+2*r)-r,
+                                  r, c, 'jeton')
+    return identifiant
+
+def mouvement_jeton(identifiant, r):
+    """Mouvement d'un jeton de rayon r"""
+    grille.move(identifiant, 0, ESPACEMENT+2*r)
+
+nb_clic = 1
+
+def on_click(event):
+    """Fonction appelée lors d'un clic sur la grille"""
+    global clic_active, nb_clic
+    if not clic_active:
+        return  # Si le clic n'est pas actif, ne rien faire
+    clic_active = False  # Désactiver le clic pendant la chute du jeton
+    colonne = int((event.x - ESPACEMENT) / (2 * RAYON + ESPACEMENT)) + 1
+    if nb_clic % 2 == 0:
+        affiche_grille_fenetre(colonne, 0, 'red')  # Couleur du jeton
+    else:
+        affiche_grille_fenetre(colonne, 0, 'yellow')  # Couleur du jeton
+    nb_clic += 1
+
+def affiche_grille_fenetre(colonne, ligneSupport, couleur):
+    """Affichage du coup joué (avec chute du pion)"""
+    global clic_active
+    ligne = NB_LIGNES
+    r = RAYON
+    identifiant = creation_jeton(colonne, ligne, couleur, r)
+    while ligne > ligneSupport:
+        if ligne < NB_LIGNES:
+            mouvement_jeton(identifiant, r)
+        fenetreJeu.update()
+        time.sleep(TEMPS_CHUTE)
+        ligne -= 1
+    clic_active = True  # Réactiver le clic une fois la chute terminée
+
+clic_active = True  # Variable pour indiquer si le clic est actif ou non
+
+grille.bind("<Button-1>", on_click)
+
 creation_grille(RAYON)
-grille.pack()  # Affichage de la grille dans la fenêtre
+grille.pack()
 
-# Lancement gui Tkinter
 fenetreJeu.mainloop()
-
